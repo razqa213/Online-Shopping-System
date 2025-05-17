d3b0d2571812ede0dbf5cc24315d71b35c8b2b90
razqa213 <razqaizzazi69@gmail.com>
2025-05-17 22:37:32 +0700

    perubahan harga produk dan output

--- Online-Shopping-System2/Electronics2.java
+++ Online-Shopping-System2/Electronics2.java
@@ -1,13 +1,28 @@
-
 public class Electronics2 extends product2 implements Discountable {
     private int warantyMonths;
+    private double price;
 
     public Electronics2() {
-        this.warantyMonths = 2;
+        this.warantyMonths = 1;
+        this.price = 0.0;
     }
 
     public void setWarantyMonths(int warantyMonths) {
         this.warantyMonths = warantyMonths;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String getCategory() {
+        return this.category;
+    }
+    // Implementasi interface Discountable
+    @Override
+    public double getDiscountedPrice(double discountPercentage) {
+        return price - (price * discountPercentage / 100);
     }
 
     @Override
@@ -15,4 +30,19 @@ public class Electronics2 extends product2 implements Discountable {
         System.out.println("Garansi: " + warantyMonths + " bulan");
     }
 
+    // Inner class Specification
+    public class Specification {
+        private double weight;
+        private String brand;
+
+        public Specification(double weight, String brand) {
+            this.weight = weight;
+            this.brand = brand;
+            
+        }
+
+        public void getSpecs() {
+            System.out.println("Spesifikasi - Berat: " + weight + " kg, Merek: " + brand); 
+        }
+    }
 }
--- Online-Shopping-System2/mainapp2.java
+++ Online-Shopping-System2/mainapp2.java
@@ -1,36 +1,39 @@
 public class mainapp2 {
     public static void main(String[] args) {
-        Electronics2 laptop = new Electronics2();
-        clothing2 baju = new clothing2();
-
-        {// mengisi object laptop
-        laptop.setName("Acer" );
-        laptop.setPrice(210.52);
-        laptop.setProductId(1);
-        laptop.setDate();
-        laptop.setWarantyMonths(12);
+        Electronics2 elektronik = new Electronics2();
+        Electronics2.Specification spek = elektronik.new Specification(2.0, "Hino");
         
+        // clothing2 baju = new clothing2();
+
+        {// mengisi object elektronik
+        elektronik.setName("Acer" );
+        elektronik.setPrice(210.00);
+        elektronik.setProductId(1);
+        elektronik.setDate();
+        elektronik.setWarantyMonths(12);
+      
+
 
         }
 
-        {
-        // mengisi object baju 
-        baju.setName("Uniqlo");
-        baju.setPrice(12.01);
-        baju.setProductId(2);
-        }
+        // {
+        // // mengisi object baju 
+        // baju.setName("Uniqlo");
+        // baju.setPrice(12.01);
+        // baju.setProductId(2);
+        // }
 
         System.out.println("");
 
         System.out.println("=== LIST PRODUK ELEKTRONIK ===");
-        System.out.println("Nama Produk : " + laptop.getName() + "\nHarga Produk : " + laptop.getPrice() + " Euro" + "\nId : " + laptop.getProductId() + "\nTanggal Pesan : " + laptop.getDateTime());
-        laptop.getInfo();
-
+        System.out.println("Nama Produk : " + elektronik.getName() + "\nHarga Produk : " + elektronik.getDiscountedPrice(10.0) + " Euro" + "\nId : " + elektronik.getProductId() + "\nTanggal Pesan : " + elektronik.getDateTime());
+        elektronik.getInfo();
+        spek.getSpecs();
         System.out.println("");
 
-        System.out.println("=== LIST PRODUK PAKAIAN ===");
-        System.out.println("Nama Produk : " + baju.getName() + "\nHarga Produk : " + baju.getPrice() + " Euro" + "\nId : " + baju.getProductId() + "\nPrice after 10% discount (Laptop): $" + baju.getDiscountedPrice(10.0));
-        baju.getInfo();
+        // System.out.println("=== LIST PRODUK PAKAIAN ===");
+        // System.out.println("Nama Produk : " + baju.getName() + "\nHarga Produk : " + baju.getPrice() + " Euro" + "\nId : " + baju.getProductId() + "\nPrice after 10% discount (Laptop): $" + baju.getDiscountedPrice(10.0));
+        // baju.getInfo();
 
     }
     
