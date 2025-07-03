52916c87b48084b936f1281338c8d9fcd7397284
Faros <faros3183@gmail.com>
2025-07-03 09:15:54 +0700

    Menambahkan folder netbeans

---
+++ ECommerce/src/main/java/com/mycompany/ecommerce/ECommerce.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ecommerce;
+
+/**
+ *
+ * @author Less__
+ */
+public class ECommerce {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
